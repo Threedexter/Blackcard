c00f4042644a62ff607dc103cf0c0da01a91b842
biepbot <biepbot@gmail.com>
2017-11-09 13:46:45 +0100

    Auto stash before merge of "master" and "origin/master"

--- Blackcard/Assets/Code/Managers/Fieldmanager.cs
+++ Blackcard/Assets/Code/Managers/Fieldmanager.cs
@@ -5,15 +5,23 @@ using UnityEngine;
 public class Fieldmanager : MonoBehaviour
 {
 
-    // Use this for initialization
-    void Start()
-    {
+    public GameObject plane;
 
+    private List<Land> lands = new List<Land>();
+
+	// Use this for initialization
+	void Awake ()
+    {
+        // todo : turn this into plane.Spawn( with parameters about land )
+        SpawnPlane(new Vector2(0, 0));
+        SpawnPlane(new Vector2(1, 0));
+        SpawnPlane(new Vector2(0, 1));
+        SpawnPlane(new Vector2(1, 1));
     }
 
-    // Update is called once per frame
-    void Update()
+    private void SpawnPlane(Vector2 location)
     {
-
+        Instantiate(plane, new Vector3(location.x, location.y, 0), plane.transform.rotation);
+        // todo : add land to list
     }
 }
